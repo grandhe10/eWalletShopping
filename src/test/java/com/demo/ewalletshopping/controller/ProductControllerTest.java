b93408a20b3ede781c76a59b8eac6ce22549f8ee
grandhe10 <sumagrandhe10@gmail.com>
2020-07-08 15:15:21 +0530

    changes

--- src/test/java/com/demo/ewalletshopping/controller/ProductControllerTest.java
+++ src/test/java/com/demo/ewalletshopping/controller/ProductControllerTest.java
@@ -1,7 +1,6 @@
 package com.demo.ewalletshopping.controller;
 
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
